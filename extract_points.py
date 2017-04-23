cb79005f07917f47f1b7ab6118399324c0c4f055
Sherdil <sniyaz@berkeley.edu>
2017-04-22 18:31:44 -0700

    Added script that extracts points and estimates normals from existing .dae mesh.

--- extract_points.py
+++ extract_points.py
@@ -1,6 +1,7 @@
 import pdb
 
 import collada
+import numpy as np
 import sys
 
 
@@ -11,8 +12,19 @@ if __name__ == "__main__":
     mesh = collada.Collada(mesh_data)
     triangles = [i for i in mesh.geometries[0].primitives[0]]
 
+    vertex_set = set()
+    for tri in triangles:
+        for vert in tri.vertices:
+            vert_tup = tuple(vert)
+            vertex_set.add(vert_tup)
+ 
+    vertex_list = [np.array(v) for v in vertex_set]
+    centroid = sum(vertex_list)
+    centroid = centroid/len(vertex_list)
+    #Hack to estimate each normal as pointing outward from center.
+    normal_list = [v - centroid for v in vertex_list] 
 
     pdb.set_trace()
-
+    
 
 
